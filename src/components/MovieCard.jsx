d88fee470015c4af7a47c1f644e0b7f06c7f7f8f
hannibal1631 <mukherjeerupak860@gmail.com>
2025-06-20 21:11:23 +0530

    add new features

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,23 +1,29 @@
-function MovieCard({movie}) {
+import '../css/MovieCard.css';
 
-    function onFavoriteClick() {
-        alert('clicked')
-    }
+function MovieCard({ movie }) {
+  function onFavoriteClick() {
+    alert('clicked');
+  }
 
-    return <div className="movie-card">
-        <div className="movie-poster">
-            <img src={movie.url} alt={movie.title} />
-            <div className="movie-overlay">
-                <button className="favorite-btn" onClick={onFavoriteClick}>
-                    ❤︎
-                </button>
-            </div>
+  return (
+    <div className='movie-card'>
+      <div className='movie-poster'>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
+        <div className='movie-overlay'>
+          <button className='favorite-btn' onClick={onFavoriteClick}>
+            ❤︎
+          </button>
         </div>
-        <div className="movie-info">
-            <h3>{movie.title}</h3>
-            <p>{movie.release_date}</p>
-        </div>
+      </div>
+      <div className='movie-info'>
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date?.split('-')[0]}</p>
+      </div>
     </div>
+  );
 }
 
-export default MovieCard
+export default MovieCard;
